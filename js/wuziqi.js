bfffd4bc63f888ec4e2d2c4180208dc8c2c88fbe
ranxu <ranxu07_18@163.com>
2015-10-05 23:19:41 +0800

    完成，剩下的就是美化及重构了

--- js/wuziqi.js
+++ js/wuziqi.js
@@ -1,6 +1,7 @@
 $().ready(function(){
 
     var i_now = 0;//除以2余数为零时黑棋，否则为白旗
+    var i_success = 0;//胜利条件
     var wuziqi = {
         init : function(){
             //  根据行列生成舞台
@@ -32,26 +33,106 @@ $().ready(function(){
             var fg_cols = cols - 0 + 1;
             for(var i = 0;i < fg_rows;i++){
                 for(var j = 0;j < fg_cols;j++){
-                    fg_html += '<div class="fg" rowno="'+i+'" colno="'+j+'"></div>';
+                    fg_html += '<div class="fg fg'+i+j+'" rowno="'+i+'" colno="'+j+'"></div>';
                 }
             }
             $('.fgs').empty().width(40*fg_cols).append(fg_html);
+            $('.container').width(40*fg_cols);
         },
         bind : function(){
+            var _this = this;
             $('.fgs').delegate('.fg','click',function(){
                 //点击首先判断是否已经有棋子了，如果没有可以生成，如果有则不能生成。黑棋先走，黑白依次变色
                 if($(this).hasClass('black')||$(this).hasClass('white')){
                     return;
                 }
+                var fgcolor = '';
                 if(i_now%2==0){
+                    fgcolor = 'black';
                     $(this).removeClass('white').addClass('black');
                 }else{
+                    fgcolor = 'white';
                     $(this).removeClass('black').addClass('white');
                 }
                 i_now++;
 
-                
+                if(_this.fn.is_ok($(this),fgcolor)){
+                   alert('成功，游戏结束！');
+                }
             });
+        },
+        fn : {
+            //fgcolor当前棋子颜色
+            is_ok : function($fg,fgcolor){
+                //成功条件，依次判断上下，左右，斜上斜下四个方向，只要有一个方向有4个跟当前同色的连一起，游戏
+                //成功
+                //console.log('begin----');
+                i_success = 0;
+                this.direct_num($fg,fgcolor,'up');
+                this.direct_num($fg,fgcolor,'down');
+                console.log(i_success+'------a');
+                if(i_success==4){//五子棋条件
+                    console.log('成功，游戏结束');
+                    return true;
+                }
+
+                i_success = 0;
+                this.direct_num($fg,fgcolor,'left');
+                this.direct_num($fg,fgcolor,'right');
+                console.log(i_success+'------b');
+                if(i_success==4){//五子棋条件
+                    console.log('成功，游戏结束');
+                    return true;
+                }
+
+                i_success = 0;
+                this.direct_num($fg,fgcolor,'leftup');
+                this.direct_num($fg,fgcolor,'rightdown');
+                console.log(i_success+'------b');
+                if(i_success==4){//五子棋条件
+                    console.log('成功，游戏结束');
+                    return true;
+                }
+
+                i_success = 0;
+                this.direct_num($fg,fgcolor,'leftdown');
+                this.direct_num($fg,fgcolor,'rightup');
+                console.log(i_success+'------b');
+                if(i_success==4){//五子棋条件
+                    console.log('成功，游戏结束');
+                    return true;
+                }
+
+                return false;
+            },
+            direct_num : function($fg,fgcolor,direct){
+                var $prev_fg =  this.get_prev_fg($fg,direct);
+                if($prev_fg && $prev_fg.length==1 && $prev_fg.hasClass(fgcolor)){
+                    i_success++;
+                    this.direct_num($prev_fg,fgcolor,direct);
+                }
+            },
+            get_prev_fg : function($fg,direct){
+                var rowno = $fg.attr('rowno');
+                var colno = $fg.attr('colno');
+                if(direct=='up'){
+                    return $('.fg'+(rowno-1)+colno);
+                }else if(direct=='down'){
+                    return $('.fg'+(rowno-0+1)+colno);
+                }else if(direct=='left'){
+                    return $('.fg'+rowno+(colno-1));
+                }else if(direct=='right'){
+                    return $('.fg'+rowno+(colno-0+1));
+                }else if(direct=='leftup'){
+                    return $('.fg'+(rowno-1)+(colno-1));
+                }else if(direct=='rightdown'){
+                    return $('.fg'+(rowno-0+1)+(colno-0+1));
+                }else if(direct=='leftdown'){
+                    return $('.fg'+(rowno-0+1)+(colno-1));
+                }else{//最后一个是rightup
+                    return $('.fg'+(rowno-1)+(colno-0+1));
+                }
+            }
         }
     };
     window.wzq = window.wzq || {};
